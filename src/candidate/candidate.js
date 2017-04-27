7d7d86c601209d1c347ff450cb4fabb8a3fc8619
Marc Intha-amnouay <marc.inthaamnouay@gmail.com>
2017-04-28 00:52:29 +0200

    merge

---
+++ src/candidate/candidate.js
@@ -0,0 +1,64 @@
+const SQLManager = require('../db/dbabstract'),
+      SQLHelper  = require('../db/dbhelper'),
+      helper     = require('../helper/helper');
+
+const candidate = (() => {
+
+    /**
+     * Ret ID 
+     * @param {*} id 
+     */
+    const retID = (id) => {
+
+        let keys;
+        let candidateData;
+
+        return new Promise((resolve, reject) => {
+            SQLManager.initDB()
+                .then(con => SQLHelper.query(con, 'SELECT * FROM candidate WHERE id = ?', [id]))
+                .then(SQLHelper.select)
+                .then(res => {
+                    candidateData = res;
+                    let jobs = JSON.parse(res[0].apply_list);
+                    keys = Object.keys(jobs);
+
+                    keys.map((d, i) => {
+                        keys[i] = d.split('_')[1];
+                    });
+
+                    return res;
+                })
+                .then(() => {
+                    let con = SQLManager.getDbInstance();
+                    let sql = 'SELECT * FROM jobs WHERE ';
+                    let param = new Array();
+
+                    keys.map((d, i) => {
+                        if (i !== keys.length - 1)
+                            sql += ' id = ? OR ';
+                        else
+                            sql += 'id = ?';  
+
+                        param.push(parseInt(d));
+                    });
+
+                    let pmSQL = SQLHelper.query(con, sql, param)
+                    return Promise.resolve(pmSQL);
+                })
+                .then(SQLHelper.select)
+                .then(res => {
+                    resolve({res, candidateData});
+                })
+                .catch(e => {
+                    console.log(e);
+                    reject(e)
+                });
+        });
+    };
+
+    return {
+        retrieveByID : retID
+    }
+})();
+
+module.exports = candidate;
